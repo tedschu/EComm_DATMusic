cbed9ace8b3e2a9059072547745ce60dd0b345a1
tedschu <ted.schuster@gmail.com>
2024-06-14 09:36:39 -0500

    updating index.js db

--- db/index.js
+++ db/index.js
@@ -1,21 +1,7 @@
-// THIS IS WHERE WE WILL WRITE OUT ALL OF THE DB FUNCTIONS (SQL CODE)
-
 const pg = require("pg");
 const bcrypt = require("bcrypt");
 
 const client = new pg.Client("postgres://localhost/DAT_music");
-
-// example of a DB function
-async function getAllUsers() {
-  try {
-    const { rows } = await client.query(`
-      SELECT * FROM users;
-    `);
-    return rows;
-  } catch (error) {
-    throw error;
-  }
-}
 
 // Ensure all DB functions are exported once completed
 module.exports = {
